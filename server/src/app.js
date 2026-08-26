4a4116a588a129015a973d1026b4104f2bdcfcf1
titiwatte06-cpu <titiwat.te06@gmail.com>
2026-08-26 16:23:44 +0700

    added-churchRequestsRouter

--- server/src/app.js
+++ server/src/app.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import churchesRouter from './routes/churches.js'
 import authRouter from './routes/auth.js'
+import churchRequestsRouter from './routes/churchRequests.js'
 
 const app = express()
 
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/churches', churchesRouter)
+app.use('/api/church-requests', churchRequestsRouter)
 app.use('/api/auth', authRouter)
 
 export default app
